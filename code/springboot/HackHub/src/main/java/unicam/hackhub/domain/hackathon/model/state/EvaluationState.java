80be03e9674c9fba91ed4b45aff3918c81aa10c7
Luca Magrini <lucamagrini1234@gmail.com>
2026-02-11 16:39:21 +0100

    Fixed valuate Submission

--- code/springboot/HackHub/src/main/java/unicam/hackhub/domain/hackathon/model/state/EvaluationState.java
+++ code/springboot/HackHub/src/main/java/unicam/hackhub/domain/hackathon/model/state/EvaluationState.java
@@ -1,6 +1,7 @@
 package unicam.hackhub.domain.hackathon.model.state;
 
 import unicam.hackhub.domain.hackathon.model.Hackathon;
+import unicam.hackhub.domain.hackathon.model.Submission;
 import unicam.hackhub.domain.hackathon.model.Valuation;
 import unicam.hackhub.domain.team.model.Team;
 
@@ -17,7 +18,17 @@ public class EvaluationState extends AbstractHackathonState {
 
     @Override
     public void valuateSubmission(String teamName, int vote, String description) {
-        this.hackathon.getSubmission(teamName).setValuation(new Valuation(vote, description));
+        Submission submission = this.hackathon.getSubmission(teamName);
+
+        if (submission == null) {
+            throw new RuntimeException("Submission not found");
+        }
+
+        if (submission.getValuation() != null) {
+            throw new RuntimeException("Submission already valuated");
+        }
+
+        submission.setValuation(new Valuation(vote, description));
     }
 
     @Override
