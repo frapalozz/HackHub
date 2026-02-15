f5dadba9802c869f6204d81d406ee57c071463ec
Francesco Palozzi <99217899+frapalozz@users.noreply.github.com>
2026-02-15 16:50:17 +0100

    Fixed SecurityConfig for h2-console

--- code/springboot/HackHub/src/main/java/unicam/hackhub/infrastructure/config/SecurityConfig.java
+++ code/springboot/HackHub/src/main/java/unicam/hackhub/infrastructure/config/SecurityConfig.java
@@ -31,7 +31,8 @@ public class SecurityConfig {
                         // Endpoint pubblici - accessibili senza autenticazione
                         .requestMatchers(
                                 "/api/v1/auth/login",
-                                "/api/v1/auth/signin"
+                                "/api/v1/auth/signin",
+                                "/h2-console/**"
                         ).permitAll()
                         // Autenticazione
                         .anyRequest().authenticated()
