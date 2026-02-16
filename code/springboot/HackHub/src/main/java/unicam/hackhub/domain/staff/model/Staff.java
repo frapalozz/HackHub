06942654a956afa45ff1052e8d2321bb86b4a755
Francesco Palozzi <99217899+frapalozz@users.noreply.github.com>
2026-02-16 10:54:00 +0100

    Changed enum type

--- code/springboot/HackHub/src/main/java/unicam/hackhub/domain/staff/model/Staff.java
+++ code/springboot/HackHub/src/main/java/unicam/hackhub/domain/staff/model/Staff.java
@@ -1,13 +1,11 @@
 package unicam.hackhub.domain.staff.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import unicam.hackhub.domain.utils.Role;
+import unicam.hackhub.domain.utils.TimeRange;
 
 @Getter
 @Setter
@@ -26,7 +24,11 @@ public class Staff {
     @Column(nullable = false)
     private String password;
 
+    @Enumerated(EnumType.STRING)
     private Role role = Role.STAFF;
+
+    @Embedded
+    private TimeRange timeRange;
 
     public Staff(String name, String email) {
         this.name = name;
--- code/springboot/HackHub/src/main/java/unicam/hackhub/domain/user/model/User.java
+++ code/springboot/HackHub/src/main/java/unicam/hackhub/domain/user/model/User.java
@@ -24,6 +24,7 @@ public class User {
     @Column(nullable = false)
     private String password;
 
+    @Enumerated(EnumType.STRING)
     private Role role = Role.USER;
 
     @ManyToOne
